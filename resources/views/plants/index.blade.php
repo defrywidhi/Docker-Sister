5fbcc0628a6b469d42ce68182d30f257af2cfcb1
romyadhy <romyady221@gmail.com>
2024-07-07 23:50:57 +0800

    fix merge bux

--- resources/views/plants/index.blade.php
+++ resources/views/plants/index.blade.php
@@ -5,38 +5,6 @@
     <h1>Plants</h1>
     <a href="{{ route('plants.create') }}" class="btn btn-primary">Add New Plant</a>
     <table class="table">
-<<<<<<< HEAD
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Scientific Name</th>
-                <th>Category</th>
-                <th>Location</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($plants as $plant)
-            <tr>
-                <td>{{ $plant->id }}</td>
-                <td>{{ $plant->name }}</td>
-                <td>{{ $plant->scientific_name }}</td>
-                <td>{{ $plant->category->name ?? 'No category' }}</td>
-                <td>{{ $plant->locations->location_name ?? 'No location' }}</td>
-                <td>
-                    <a href="{{ route('plants.show', $plant->id) }}" class="btn btn-info">View</a>
-                    <a href="{{ route('plants.edit', $plant->id) }}" class="btn btn-warning">Edit</a>
-                    <form action="{{ route('plants.destroy', $plant->id) }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-=======
     <thead>
     <tr>
         <th>ID</th>
@@ -75,7 +43,6 @@
                 </tr>
                 @endforeach
             </tbody>
->>>>>>> 54bcca1b3485505cabdb6c5decb73eb64668865d
     </table>
 </div>
 @endsection
